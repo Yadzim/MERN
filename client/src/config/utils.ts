bc1bd8d524abccaa405c6ec342cd84e38f79a9b0
Eric <82119304+Yadzim@users.noreply.github.com>
2024-01-27 19:52:42 +0500

    global url

--- client/src/config/utils.ts
+++ client/src/config/utils.ts
@@ -1,9 +1,8 @@
 
-// export const URL = "http://192.168.179.207:8090/OnlineShop/aj";
-export const MAIN_URL = "http://192.168.179.207:8090/OnlineShop/aj";
+export const MAIN_URL = "http://localhost:5000/api";
 
-export const URL = "http://192.168.137.48:5000/api"  // Ahror
+export const URL = "http://localhost:5000//api";
 // export const URL = "http://192.168.137.117:5000/api"  // Ahror
 // export const URL = "http://192.168.11.11:5000/api"    // Azizxon
-// export const URL = "http://192.168.179.11:5000/api"    // Azizxon ...
-// export const URL = "http://192.168.1.92:5000/api"    // Yadzim ...
+// export const URL = "http://192.168.179.11:5000/api"    // Azizxon
+// export const URL = "http://192.168.1.92:5000/api"    // Yadzim
